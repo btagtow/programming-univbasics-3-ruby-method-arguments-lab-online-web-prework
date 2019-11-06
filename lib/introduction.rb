e109dfb6fa1ed2c7830d822770251f245f8fcb03
Ben Tagtow <bentagtow@gmail.com>
2019-11-06 18:12:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,10 +6,10 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 def introduction_with_language (name,language)
-  print "Hi, my name is #{name} and I am learning to program in #{langage}."
+  puts "Hi, my name is #{name} and I am learning to program in #{langage}."
 end
 def introduction_with_language (name,language=Ruby)
-  print "Hi, my name is #{name} and I am learning to program in #{langage}."
+  puts "Hi, my name is #{name} and I am learning to program in #{langage}."
 end
   
 
