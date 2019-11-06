eb75b46a768f603de43a1f576aa74bfdac0b20f9
Ben Tagtow <bentagtow@gmail.com>
2019-11-06 18:14:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,15 +5,14 @@
 def introduction (name)
   puts "Hi, my name is #{name}."
 end
-introduction
 def introduction_with_language (name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{langage}."
 end
-introduction_with_language
+
 def introduction_with_language (name,language=Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{langage}."
 end
-introduction_with_language_optional
+
   
 
 
