eff3c97a8a7f4a0d7629df2a7e971f3a727df325
Ben Tagtow <bentagtow@gmail.com>
2019-11-06 18:12:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,13 +2,13 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-def introduction(name)
+def introduction (name)
   print "Hi, my name is #{name}."
 end
-def introduction_with_language(name,language)
+def introduction_with_language (name,language)
   print "Hi, my name is #{name} and I am learning to program in #{langage}."
 end
-def introduction_with_language(name,language=Ruby)
+def introduction_with_language (name,language=Ruby)
   print "Hi, my name is #{name} and I am learning to program in #{langage}."
 end
   
